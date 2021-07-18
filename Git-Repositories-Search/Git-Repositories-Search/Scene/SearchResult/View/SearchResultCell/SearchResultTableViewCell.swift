2d5915ee555f73a037b72015739caefb2842ae56
AbdulrhmanNoon <abdelrhmanameennoon@gmail.com>
2021-07-18 11:25:17 +0200

    Finished SearchResultTableViewCell

---
+++ Git-Repositories-Search/Git-Repositories-Search/Scene/SearchResult/View/SearchResultCell/SearchResultTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  SearchResultTableViewCell.swift
+//  Git-Repositories-Search
+//
+//  Created by AbdulrhmanNoon on 5/9/21.
+//
+
+import UIKit
+import Kingfisher
+
+class SearchResultTableViewCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+    @IBOutlet private weak var userImageView: UIImageView!
+    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var itemNameLabel: UILabel!
+    @IBOutlet private weak var itemDescriptionLabel: UILabel!
+    @IBOutlet private weak var starsCountLabel: UILabel!
+    @IBOutlet private weak var languageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
+
+extension SearchResultTableViewCell {
+    func configure(with repo: SearchResultModel) {
+        let url = URL(string: repo.owner.avatar_url)
+        self.userImageView.kf.setImage(with: url)
+        self.userNameLabel.text = repo.owner.login
+        self.itemNameLabel.text = repo.name
+        self.itemDescriptionLabel.text = repo.description
+        self.starsCountLabel.text = "\(repo.stargazers_count)"
+        self.languageLabel.text = repo.language
+    }
+    
+}
